b374858edc31dba7abc0d10fec402e30eb4eabe6
zhouguangyin <1176006875@qq.com>
2023-11-22 11:31:26 +0800

    Market

--- sync-node/src/main/java/io/trxplorer/syncnode/service/MarketSyncService.java
+++ sync-node/src/main/java/io/trxplorer/syncnode/service/MarketSyncService.java
@@ -23,119 +23,128 @@ import io.trxplorer.model.tables.pojos.Market;
 
 public class MarketSyncService {
 
-	private DSLContext dslContext;
+    private DSLContext dslContext;
 
-	private static final Logger logger = LoggerFactory.getLogger(MarketSyncService.class);
-	
-	@Inject
-	public MarketSyncService(DSLContext dslContext) {
-		this.dslContext = dslContext;
-	}
+    private static final Logger logger = LoggerFactory.getLogger(MarketSyncService.class);
 
-	
-	public void syncMarkets() {
-		
-		try {
-			
-			Document res = Jsoup.connect("https://coinmarketcap.com/currencies/tron").get();
-			
-			String price = res.select("#quote_price").first().attr("data-usd");
-			String avolume = res.select("span[data-currency-volume]").first().attr("data-usd");
-			
-			Market amarket = new Market();
-			
-			amarket.setPair("TRX/USD");
-			amarket.setSource("CMC");
-			amarket.setVolume_24h(UInteger.valueOf((int)Double.parseDouble(avolume)));
-			amarket.setPrice(BigDecimal.valueOf(Double.valueOf(price)));
-			
-			createOrUpdate(amarket);
-			
-			Elements elements = res.select("#markets-table tbody tr");
-			
-			System.out.println("==>"+elements.size());
-			
-			Iterator<Element> it = elements.iterator();
-			
-			while(it.hasNext()) {
-				
-				Element tr = it.next();
-				Elements columns = tr.select("td");
-				
-				String source = columns.get(1).text();
-				String pair = columns.get(2).text();
-				String mvolume = columns.get(3).text().replace("$", "").replace(",", "");
-				String mprice =  columns.get(4).text().replace("$", "").replace(",", "").replace("*", "");
-				String updated = columns.get(8).text();
-				
+    @Inject
+    public MarketSyncService(DSLContext dslContext) {
+        this.dslContext = dslContext;
+    }
 
-				if (!updated.toLowerCase().equals("recently") ||mvolume.contains("*")) {
-					continue;
-				}
-				
-				Market market = new Market();
-				
-				market.setPair(pair);
-				market.setSource(source);
-				market.setVolume_24h(UInteger.valueOf((int)Double.parseDouble(mvolume)));
-				market.setPrice(BigDecimal.valueOf(Double.valueOf(mprice)));				
-				
-				createOrUpdate(market);
-			}
-			
+    public void syncMarkets() {
 
-		}catch(IOException e) {
-			logger.error("Error while fetching markets data",e);
-		}
-		
-	}
+        //先写死，之前的取值数据不对。
+        Market amarket = new Market();
 
-	
-	private void createOrUpdate(Market market) {
-		
-		int day = LocalDateTime.now().getDayOfMonth();
-		int month = LocalDateTime.now().getMonthValue();
-		int year = LocalDateTime.now().getYear();
-		
-		UInteger id = this.dslContext.select(MARKET.ID)
-		.from(MARKET)
-		.where(MARKET.DAY.eq(day)
-				.and(MARKET.MONTH.eq(month))
-				.and(MARKET.YEAR.eq(year)))
-				.and(MARKET.SOURCE.eq(market.getSource()))
-				.and(MARKET.PAIR.eq(market.getPair()))
-		.fetchOneInto(UInteger.class);
-		
-		if (id==null) {
-			
-			this.dslContext.insertInto(MARKET)
-			.set(MARKET.DAY,day)
-			.set(MARKET.MONTH,month)
-			.set(MARKET.YEAR,year)
-			.set(MARKET.PAIR,market.getPair())
-			.set(MARKET.SOURCE,market.getSource())
-			.set(MARKET.VOLUME_24H,market.getVolume_24h())
-			.set(MARKET.PRICE,market.getPrice())
-			.set(MARKET.LAST_UPDATE,Timestamp.valueOf(LocalDateTime.now()))
-			.execute();
-			
-		}else {
-			
-			this.dslContext.update(MARKET)
-			.set(MARKET.PAIR,market.getPair())
-			.set(MARKET.SOURCE,market.getSource())
-			.set(MARKET.VOLUME_24H,market.getVolume_24h())
-			.set(MARKET.PRICE,market.getPrice())
-			.set(MARKET.LAST_UPDATE,Timestamp.valueOf(LocalDateTime.now()))
-			.where(MARKET.ID.eq(id))
-			.execute();			
-			
-			
-		}
-		
-	}
+        amarket.setPair("TRX/USD");
+        amarket.setSource("CMC");
+        amarket.setVolume_24h(UInteger.valueOf(100));
+        amarket.setPrice(new BigDecimal("0.09"));
 
-	
+        createOrUpdate(amarket);
 
-	
+//        try {
+
+//            Document res = Jsoup.connect("https://coinmarketcap.com/currencies/tron").get();
+//            //价格
+//            //成交数量,的价格
+//            String price = res.select("#quote_price").first().attr("data-usd");
+//            String avolume = res.select("span[data-currency-volume]").first().attr("data-usd");
+//
+//            Market amarket = new Market();
+//
+//            amarket.setPair("TRX/USD");
+//            amarket.setSource("CMC");
+//            amarket.setVolume_24h(UInteger.valueOf((int) Double.parseDouble(avolume)));
+//            amarket.setPrice(BigDecimal.valueOf(Double.valueOf(price)));
+//
+//            createOrUpdate(amarket);
+//
+//            Elements elements = res.select("#markets-table tbody tr");
+//
+//            System.out.println("==>" + elements.size());
+//
+//            Iterator<Element> it = elements.iterator();
+//
+//            while (it.hasNext()) {
+//
+//                Element tr = it.next();
+//                Elements columns = tr.select("td");
+//
+//                String source = columns.get(1).text();
+//                String pair = columns.get(2).text();
+//                String mvolume = columns.get(3).text().replace("$", "").replace(",", "");
+//                String mprice = columns.get(4).text().replace("$", "").replace(",", "").replace("*", "");
+//                String updated = columns.get(8).text();
+//
+//
+//                if (!updated.toLowerCase().equals("recently") || mvolume.contains("*")) {
+//                    continue;
+//                }
+//
+//                Market market = new Market();
+//                //交易对TRX/USD
+//                market.setPair(pair);
+//                //消息来源
+//                market.setSource(source);
+//                //24小时成交数量,的价格
+//                market.setVolume_24h(UInteger.valueOf((int) Double.parseDouble(mvolume)));
+//                //价格
+//                market.setPrice(BigDecimal.valueOf(Double.valueOf(mprice)));
+//
+//                createOrUpdate(market);
+//            }
+
+
+//        } catch (IOException e) {
+//            logger.error("Error while fetching markets data", e);
+//        }
+
+    }
+
+
+    private void createOrUpdate(Market market) {
+
+        int day = LocalDateTime.now().getDayOfMonth();
+        int month = LocalDateTime.now().getMonthValue();
+        int year = LocalDateTime.now().getYear();
+
+        UInteger id = this.dslContext.select(MARKET.ID)
+                .from(MARKET)
+                .where(MARKET.DAY.eq(day)
+                        .and(MARKET.MONTH.eq(month))
+                        .and(MARKET.YEAR.eq(year)))
+                .and(MARKET.SOURCE.eq(market.getSource()))
+                .and(MARKET.PAIR.eq(market.getPair()))
+                .fetchOneInto(UInteger.class);
+
+        if (id == null) {
+
+            this.dslContext.insertInto(MARKET)
+                    .set(MARKET.DAY, day)
+                    .set(MARKET.MONTH, month)
+                    .set(MARKET.YEAR, year)
+                    .set(MARKET.PAIR, market.getPair())
+                    .set(MARKET.SOURCE, market.getSource())
+                    .set(MARKET.VOLUME_24H, market.getVolume_24h())
+                    .set(MARKET.PRICE, market.getPrice())
+                    .set(MARKET.LAST_UPDATE, Timestamp.valueOf(LocalDateTime.now()))
+                    .execute();
+
+        } else {
+
+            this.dslContext.update(MARKET)
+                    .set(MARKET.PAIR, market.getPair())
+                    .set(MARKET.SOURCE, market.getSource())
+                    .set(MARKET.VOLUME_24H, market.getVolume_24h())
+                    .set(MARKET.PRICE, market.getPrice())
+                    .set(MARKET.LAST_UPDATE, Timestamp.valueOf(LocalDateTime.now()))
+                    .where(MARKET.ID.eq(id))
+                    .execute();
+
+        }
+
+    }
+
 }
